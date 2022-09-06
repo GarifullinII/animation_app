7aed7a3ca2631350f33e93c36dca4d3a465221d7
GArifullinII <ildaridrisovich@gmail.com>
2022-09-06 13:51:34 +0400

    Initial Commit

---
+++ animation_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  animation_app
+//
+//  Created by Ildar Garifullin on 06/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
